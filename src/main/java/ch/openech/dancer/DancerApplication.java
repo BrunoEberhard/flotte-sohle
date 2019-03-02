46ab8751a0ed687d0c566831ff3c1b9c11ff0595
Marco Musolf <marcomusolf@hotmail.com>
2019-03-02 15:19:56 +0100

    Test git configuration 2

--- src/main/java/ch/openech/dancer/DancerApplication.java
+++ src/main/java/ch/openech/dancer/DancerApplication.java
@@ -26,7 +26,6 @@ import ch.openech.dancer.frontend.UserTablePage;
 import ch.openech.dancer.model.DanceEvent;
 import ch.openech.dancer.model.Location;
 
-/*Test*/
 public class DancerApplication extends Application {
 	
 	@Override
