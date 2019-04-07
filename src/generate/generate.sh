eddb7291e9c8e69760230d856648e204d31d3aad
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-04-07 14:55:04 +0200

    cleanup

--- src/generate/generate.sh
+++
@@ -1,4 +0,0 @@
-# integrate: https://stackoverflow.com/questions/38532210/how-to-integrate-angular-2-java-maven-web-application
-# generator: https://github.com/OpenAPITools/openapi-generator
-
-java -jar openapi-generator-cli-3.3.1.jar generate -i swagger.json -g typescript-angular -o client
--- src/main/resources/sql/delete_tables.sql
+++
@@ -1,6 +0,0 @@
-delete from  dancer.DANCE_EVENT;
-delete from  dancer.USER___ROLES;
-delete from  dancer.USER_;
-delete from  dancer.RECUR;
-delete from  dancer.ORGANIZER;
-delete from  dancer.LOCATION;
