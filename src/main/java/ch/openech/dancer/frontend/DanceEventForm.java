dda4ae0cbccc36cc9bd3830b15255d635592c93e
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-05-04 10:00:44 +0200

    Eintritt anzeigen

--- src/main/java/ch/openech/dancer/frontend/DanceEventForm.java
+++ src/main/java/ch/openech/dancer/frontend/DanceEventForm.java
@@ -24,6 +24,7 @@ public class DanceEventForm extends Form<DanceEvent> {
 		form.line($.description);
 		// form.line(new FlyerFormElement($.flyer, editable));
 		form.line(locationEditable ? new ReferenceFormElement<>($.location, Location.$.name) : new TextFormElement($.location));
+		form.line($.price, $.priceReduced);
 		form.line($.tags);
 		if (!editable) {
 			form.line($.status);
