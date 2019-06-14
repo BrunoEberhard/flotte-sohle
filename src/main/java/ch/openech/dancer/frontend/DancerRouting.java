a0106d62d153a6bcdd6da1ecb93e0bc232b82086
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-06-14 17:33:43 +0200

    DancerRouting: Fehler nicht abfangen
    Dafür ist das framework zuständig

--- src/main/java/ch/openech/dancer/frontend/DancerRouting.java
+++ src/main/java/ch/openech/dancer/frontend/DancerRouting.java
@@ -1,30 +1,20 @@
 package ch.openech.dancer.frontend;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.minimalj.application.Application;
 import org.minimalj.frontend.page.Page;
 import org.minimalj.frontend.page.Routing;
 import org.minimalj.util.StringUtils;
 
 public class DancerRouting extends Routing {
-	private static final Logger logger = Logger.getLogger(Routing.class.getName());
 
 	@Override
 	protected Page createPage(String route) {
-		try {
-			if (route != null && route.startsWith("event/")) {
-				String id = route.substring("event/".length());
-				return new EventPage(id);
-			} else if (StringUtils.equals(route, "map")) {
-				return new LocationMapPage();
-			}
-		} catch (Exception x) {
-			logger.log(Level.WARNING, x.getClass().getSimpleName() + ": " + route);
-
+		if (route != null && route.startsWith("event/")) {
+			String id = route.substring("event/".length());
+			return new EventPage(id);
+		} else if (StringUtils.equals(route, "map")) {
+			return new LocationMapPage();
 		}
-		return Application.getInstance().createDefaultPage();
+		return null;
 	}
 
 	@Override
