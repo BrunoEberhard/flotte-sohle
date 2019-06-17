e61ae397613ebead0085bee93736f983e111e72f
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-06-17 21:01:09 +0200

    LocationMapPage cleanup

--- src/main/java/ch/openech/dancer/frontend/LocationMapPage.java
+++ src/main/java/ch/openech/dancer/frontend/LocationMapPage.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.apache.http.client.utils.URIBuilder;
 import org.minimalj.backend.Backend;
@@ -15,74 +16,72 @@ import org.minimalj.frontend.Frontend.IContent;
 import org.minimalj.frontend.impl.json.JsonFrontend;
 import org.minimalj.frontend.impl.json.JsonReader;
 import org.minimalj.frontend.impl.json.JsonWriter;
-import org.minimalj.frontend.page.HtmlPage;
+import org.minimalj.frontend.page.Page;
 import org.minimalj.repository.query.By;
-import org.minimalj.util.resources.Resources;
 
+import ch.openech.dancer.DancerWebServer;
 import ch.openech.dancer.model.Location;
 
-public class LocationMapPage extends HtmlPage {
+public class LocationMapPage extends Page {
+	private static final Logger LOG = Logger.getLogger(DancerWebServer.class.getName());
 
 	private static String template;
+
 	static {
 		template = JsonFrontend.readStream(LocationMapPage.class.getResourceAsStream("/ch/openech/dancer/location_map.html"));
 	}
 
-	public LocationMapPage() {
-		super(template, "Tanzkarte");
-	}
-
-	public String getTitle() {
-		return Resources.getString(getClass());
-	}
-
 	@Override
 	public IContent getContent() {
-		String htmlOrUrl = JsonFrontend.readStream(LocationMapPage.class.getResourceAsStream("/ch/openech/dancer/location_map.html"));
-
 		List<Location> locations = Backend.find(Location.class, By.all());
 
 		// https://nominatim.openstreetmap.org/search?country=CH&city=Bern&format=jsonv2
 
 		List<Map<String, Object>> locs = new ArrayList<>();
-		for (Location l : locations) {
+		for (Location location : locations) {
 			Map<String, Object> lMap = new HashMap<>();
-			lMap.put("name", l.name);
-			lMap.put("address", l.address);
-			lMap.put("city", l.city);
-			lMap.put("url", l.url);
+			lMap.put("name", location.name);
+			lMap.put("address", location.address);
+			lMap.put("city", location.city);
+			lMap.put("url", location.url);
 
-			if (l.latitude == null) {
-				try {
-					URIBuilder b = new URIBuilder("https://nominatim.openstreetmap.org");
-					b.addParameter("country", l.country);
-					b.addParameter("city", l.city.substring(l.city.indexOf(' ') + 1));
-					b.addParameter("street", l.address);
-					b.addParameter("format", "jsonv2");
-
-					URL url = b.build().toURL();
-					try (InputStreamReader isr = new InputStreamReader(url.openStream())) {
-						List result = (List) JsonReader.read(isr);
-						Map<String, Object> values = (Map<String, Object>) result.get(0);
-						l.latitude = new BigDecimal((String) values.get("lat"));
-						l.longitude = new BigDecimal((String) values.get("lon"));
-					}
-					Backend.update(l);
-				} catch (Exception x) {
-					x.printStackTrace();
-				}
+			if (location.latitude == null) {
+				location = getPosition(location);
 			}
-			lMap.put("latitude", l.latitude);
-			lMap.put("longitude", l.longitude);
-
-			locs.add(lMap);
+			if (location.latitude != null) {
+				lMap.put("latitude", location.latitude);
+				lMap.put("longitude", location.longitude);
+				locs.add(lMap);
+			}
 		}
 
 		String json = new JsonWriter().write(locs);
 
-		htmlOrUrl = htmlOrUrl.replace("$LOCS", json);
+		String html = template.replace("$LOCS", json);
+		return Frontend.getInstance().createHtmlContent(html);
+	}
 
-		return Frontend.getInstance().createHtmlContent(htmlOrUrl);
+	private Location getPosition(Location location) {
+		try {
+			URIBuilder b = new URIBuilder("https://nominatim.openstreetmap.org");
+			b.addParameter("country", location.country);
+			b.addParameter("city", location.city.substring(location.city.indexOf(' ') + 1));
+			b.addParameter("street", location.address);
+			b.addParameter("format", "jsonv2");
+
+			URL url = b.build().toURL();
+			try (InputStreamReader isr = new InputStreamReader(url.openStream())) {
+				List<?> result = (List<?>) JsonReader.read(isr);
+				@SuppressWarnings("unchecked")
+				Map<String, Object> values = (Map<String, Object>) result.get(0);
+				location.latitude = new BigDecimal((String) values.get("lat"));
+				location.longitude = new BigDecimal((String) values.get("lon"));
+			}
+			return Backend.save(location);
+		} catch (Exception x) {
+			LOG.warning("Could not retrieve position of " + location.name + " (" + x.getMessage() + ")");
+			return location;
+		}
 	}
 
 }
