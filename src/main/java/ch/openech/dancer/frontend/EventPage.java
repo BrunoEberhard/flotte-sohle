14903fcc70b0be4a0404723ffe5d876b10b94343
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-06-18 21:57:32 +0200

    ld+json als schema info verwenden

--- src/main/java/ch/openech/dancer/frontend/EventPage.java
+++ src/main/java/ch/openech/dancer/frontend/EventPage.java
@@ -1,14 +1,20 @@
 package ch.openech.dancer.frontend;
 
-import java.time.LocalDateTime;
+import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.minimalj.backend.Backend;
 import org.minimalj.frontend.impl.json.JsonFrontend;
+import org.minimalj.frontend.impl.json.JsonWriter;
 import org.minimalj.frontend.page.HtmlPage;
 import org.minimalj.util.StringUtils;
 
 import ch.openech.dancer.model.DanceEvent;
+import ch.openech.dancer.model.DeeJay;
+import ch.openech.dancer.model.Location;
 
 public class EventPage extends HtmlPage {
 
@@ -56,16 +62,92 @@ public class EventPage extends HtmlPage {
 			result = result.replace("$city", "");
 		}
 
-		String dateText = event.getDayOfWeek() + ", " + shortFormat.format(event.date);
-		result = result.replace("$dateText", dateText);
-
-		String date = LocalDateTime.of(event.date, event.from).toString();
+		String date = event.getDayOfWeek() + ", " + shortFormat.format(event.date);
 		result = result.replace("$date", date);
 
 		if (!StringUtils.isEmpty(event.location.url)) {
 			result = result.replace("$url", event.location.url);
 		}
+		
+		result = result.replace("$json", createJson(event));
 		return result;
 	}
+	
+	private static String createJson(DanceEvent event) {
+		JsonWriter writer = new JsonWriter();
+		Map<String, Object> values = new LinkedHashMap<>();
+		values.put("@context", "http://schema.org");
+		values.put("@type", "DanceEvent");
+		if (event.location != null) {
+			values.put("organizer", createJsonOrganization(event.location));
+			values.put("location", createJsonPlace(event.location));
+		}
+		values.put("name", event.title);
+		if (event.price != null) {
+			values.put("offers", createJsonOffer(event.price));
+		}
+		values.put("startDate", event.date.toString());
+		values.put("endDate", event.date.toString());
+		if (event.description != null) {
+			values.put("description", event.description);
+		}
+		values.put("doorTime", event.from.toString());
+		if (event.until != null) {
+			Duration duration = Duration.between(event.from, event.until);
+			if (duration.isNegative()) {
+				duration = duration.plusDays(1);
+			}
+			values.put("duration", duration.toString());
+		}
+		if (event.deeJay != null) {
+			values.put("performer", createJsonPerson(event.deeJay));
+		}
+		values.put("image", "https://www.flotte-sohle.ch/sohle_rot.png");
+		values.put("url", "https://www.flotte-sohle.ch/event/" + event.id);
+		return writer.write(values);
+	}
+	
+	private static Map<String, Object> createJsonOrganization(Location location) {
+		Map<String, Object> values = new LinkedHashMap<>();
+		values.put("@type", "Organization");
+		values.put("name", location.name);
+		values.put("url", location.url);
+		return values;
+	}
+	
+	private static Map<String, Object> createJsonPlace(Location location) {
+		Map<String, Object> values = new LinkedHashMap<>();
+		values.put("@type", "Place");
+		values.put("name", location.name);
+		values.put("address", createJsonPostalAddress(location));
+		return values;
+	}
+	
+	private static Map<String, Object> createJsonPostalAddress(Location location) {
+		Map<String, Object> values = new LinkedHashMap<>();
+		values.put("@type", "PostalAddress");
+		values.put("addressLocality", location.city);
+		values.put("streetAddress", location.address);
+		values.put("addressCountry", location.country);
+		return values;
+	}
 
+	private static Map<String, Object> createJsonOffer(BigDecimal price) {
+		Map<String, Object> values = new LinkedHashMap<>();
+		values.put("@type", "Offer");
+		values.put("price", price.toPlainString());
+		values.put("priceCurrency", "CHF");
+		return values;
+	}
+	
+	private static Map<String, Object> createJsonPerson(DeeJay deeJay) {
+		Map<String, Object> values = new LinkedHashMap<>();
+		values.put("@type", "Person");
+		values.put("name", deeJay.name);
+		if (deeJay.url != null) {
+			values.put("url", deeJay.url);
+		}
+		return values;
+	}
+	
 }
