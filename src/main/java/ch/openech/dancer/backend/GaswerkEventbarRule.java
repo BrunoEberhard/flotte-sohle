bcab32dac0f0fa0f850641cc49fcd7274328ed13
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-07-14 13:14:37 +0200

    GaswerkEventbarRule

---
+++ src/main/java/ch/openech/dancer/backend/GaswerkEventbarRule.java
@@ -0,0 +1,29 @@
+package ch.openech.dancer.backend;
+
+public class GaswerkEventbarRule {
+
+	// https://www.gaswerk-eventbar.ch/
+
+	// jeden sonntag ab 18.8.2019. Anfrage aber noch offen
+
+//	GASWERK EVENTBAR GMBH
+//	BAHNHOFSTRASSE 180B
+//	6423 SEEWEN
+
+//	DATUM SONNTAG, 18.08.2019
+//	TÜRÖFFNUNG 19.30 UHR
+//	BEGINN 20.00 UHR
+//	ART PAARTANZABEND
+//	EINTRITT EINTRITT FREI / TOPFKOLLEKTE
+
+//	Innert kürzester Zeit hat sich die Paartanzreihe «Tanzwerk» im Gaswerk als fester Bestandteil im überregionalen Angebot für Paartanzliebhaber etabliert. Von Discofox, Walzer, Rock`n`Roll und Jive über Salsa, Bachata bis hin zu Chachacha und Swing wird alles geboten was das Tänzerherz höherschlagen lässt.
+//
+//	Strukturiert in Tanzstil-Blöcke wird durch den Abend geführt. Dabei garantieren wir rhythmisches Vergnügen durch sämtliche Tanzstile.
+//
+//	Geboten wird ein bunter Mix von aktuellen Hits, bis hin zu Klassikern aus vergangenen Jahren in den Bereichen: Pop, Classics, Rock, EDM, Swing, R&B bis hin zu Chanson und Schlager. Das stetig wachsende und altersübergreifende Publikum bestätigt: Paartanz ist angesagter den je!
+//
+//	Kein Tanzpartner? kein Problem. Gerne bittet unser Gaswerk-Tänzer unsere weiblichen Gäste ohne Begleitung aufs Parkett. Lasst euch von der Musik mitreissen und tretet ein ins «Tanzwerk». Dem Paartanztreffpunkt eurer Region.
+//
+//	Gerne erfüllen wir auch Musik- und Tanzstilwünsche direkt ab dem Tanzparkett um den Abend für euch unvergesslich zu machen. Der Eintritt zum Vergnügen ist kostenlos!
+
+}
