dd5ce13f9d4f6a33e56a4d3f7e47412ce5a5a28a
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-07-20 08:24:38 +0200

    DanceEventAdminTablePage: filter

--- src/main/java/ch/openech/dancer/frontend/DanceEventAdminTablePage.java
+++ src/main/java/ch/openech/dancer/frontend/DanceEventAdminTablePage.java
@@ -6,15 +6,21 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.minimalj.backend.Backend;
+import org.minimalj.frontend.Frontend.FormContent;
 import org.minimalj.frontend.action.Action;
 import org.minimalj.frontend.form.Form;
+import org.minimalj.frontend.form.element.ComboBoxFormElement;
 import org.minimalj.frontend.page.SimpleTableEditorPage;
+import org.minimalj.model.Keys;
 import org.minimalj.model.validation.ValidationMessage;
 import org.minimalj.repository.query.By;
+import org.minimalj.repository.query.Criteria;
+import org.minimalj.repository.query.Order;
 import org.minimalj.util.resources.Resources;
 
 import ch.openech.dancer.model.DanceEvent;
 import ch.openech.dancer.model.EventStatus;
+import ch.openech.dancer.model.Location;
 
 public class DanceEventAdminTablePage extends SimpleTableEditorPage<DanceEvent> {
 
@@ -39,9 +45,37 @@ public class DanceEventAdminTablePage extends SimpleTableEditorPage<DanceEvent> 
 		}
 	}
 
+	private final EventFilter filter = new EventFilter();
+
+	@Override
+	protected FormContent getOverview() {
+		Form<EventFilter> form = new Form<>(2);
+		List<Location> locations = Backend.find(Location.class, By.all().order(Location.$.name));
+		form.line(new ComboBoxFormElement<Location>(Keys.getProperty(EventFilter.$.location), locations));
+		form.setChangeListener(source -> refresh());
+		form.setObject(filter);
+		return form.getContent();
+	}
+
+	public static class EventFilter {
+		public static final EventFilter $ = Keys.of(EventFilter.class);
+
+		public Location location;
+
+		public LocalDate date;
+	}
+
 	@Override
 	protected List<DanceEvent> load() {
-		return Backend.find(DanceEvent.class, By.all().order(DanceEvent.$.location).order(DanceEvent.$.date));
+		Criteria criteria = By.ALL;
+		if (filter.date != null) {
+			criteria = criteria.and(By.field(EventFilter.$.date, filter.date));
+		}
+		if (filter.location != null) {
+			criteria = criteria.and(By.field(EventFilter.$.location, filter.location));
+		}
+		Order order = criteria.order(DanceEvent.$.location).order(DanceEvent.$.date);
+		return Backend.find(DanceEvent.class, order);
 	}
 
 	@Override
