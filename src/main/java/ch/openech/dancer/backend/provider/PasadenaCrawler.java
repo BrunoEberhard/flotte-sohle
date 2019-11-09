630a6f2d9eff646000d31953e6441fe10e252617
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-11-09 16:12:03 +0100

    PasadenaCrawler aktualisiert

--- src/main/java/ch/openech/dancer/backend/provider/PasadenaCrawler.java
+++ src/main/java/ch/openech/dancer/backend/provider/PasadenaCrawler.java
@@ -27,7 +27,7 @@ import ch.openech.dancer.model.Region;
 public class PasadenaCrawler extends DanceEventProvider {
 	private static final long serialVersionUID = 1L;
 
-	private static final String AGENDA_URL = "http://www.pasadena.ch/agendanews/";
+	private static final String AGENDA_URL = "https://www.pasadena.ch/agenda-news-club-dancing/";
 
 	private static final String[] TITLES = { "Schlagerparty", "Schlagernacht", "Facebookparty" };
 
@@ -36,12 +36,18 @@ public class PasadenaCrawler extends DanceEventProvider {
 		EventUpdateCounter result = new EventUpdateCounter();
 
 		Document doc = Jsoup.connect(AGENDA_URL).userAgent(USER_AGENT).get();
-		Element c31 = doc.selectFirst("#c31");
-		Elements elements = c31.select("p[style]");
+		Elements elements = doc.select(".post-item");
 		for (Element element : elements) {
-			if (!isSimpleElement(element)) {
-				LocalDate date = extractLocalDate(element);
-				LocalTime[] period = extractPeriod(element);
+			try {
+
+				Element elementPost = element.selectFirst(".post-excerpt");
+				String text = elementPost.text();
+
+				LocalDate date = extractLocalDate(text);
+				LocalTime[] period = extractPeriod(text);
+				String djText = extractDj(text);
+				BigDecimal price = extractPrice(text);
+
 				if (DanceEvent.isDuringTheDay(period[0]))
 					continue;
 
@@ -57,18 +63,19 @@ public class PasadenaCrawler extends DanceEventProvider {
 					continue;
 				}
 
+				Element elementTitle = element.selectFirst(".entry-title");
+
 				danceEvent.status = EventStatus.generated;
 				danceEvent.date = date;
 				danceEvent.header = location.name;
-				danceEvent.title = extractDanceEventTitle(element);
+				danceEvent.title = elementTitle.text();
 				danceEvent.from = period[0];
 				danceEvent.until = period[1];
-				danceEvent.description = element.nextElementSibling().text();
+				danceEvent.description = text;
 				danceEvent.location = location;
+				danceEvent.price = price;
 
-				Elements mitDj = element.nextElementSibling().getElementsContainingOwnText("Mit DJ");
-				if (!mitDj.isEmpty()) {
-					String djText = mitDj.get(0).ownText().substring(4);
+				if (!StringUtils.isEmpty(djText)) {
 					danceEvent.deeJay = getDeeJay(djText);
 				} else {
 					danceEvent.deeJay = null;
@@ -89,42 +96,50 @@ public class PasadenaCrawler extends DanceEventProvider {
 				}
 
 				save(danceEvent, result);
+			} catch (Exception x) {
+				result.failedEvents++;
+				result.exception = x.getMessage();
 			}
 		}
 		return result;
+
 	}
 
-	private boolean isSimpleElement(Element element) {
-		return element.nextElementSibling() == null || element.childNodeSize() != 3;
-	}
+	// Dienstag 12. November 19:30 bis 24:00 mit DJ Rolf Eintritt (CHF): 10.00
+	// (Member gratis)
 
-	private LocalTime[] extractPeriod(Element element) {
-		if (element.childNodeSize() == 3) {
-			String period = element.childNode(2).toString();
-			if (period != null) {
-				String[] moments = period.split("bis");
-				if (moments.length == 2) {
-					return new LocalTime[] { LocalTime.parse(moments[0].trim(), DateUtils.TIME_FORMAT), LocalTime.parse(moments[1].trim(), DateUtils.TIME_FORMAT) };
-				}
-			}
+	private LocalTime[] extractPeriod(String text) {
+		int index = text.indexOf(" bis ");
+		if (index > 5 && index < text.length() - 10) {
+			return new LocalTime[] { LocalTime.parse(text.substring(index - 5, index), DateUtils.TIME_FORMAT), LocalTime.parse(text.substring(index + 5, index + 10), DateUtils.TIME_FORMAT) };
 		}
 		return null;
 	}
 
-	private LocalDate extractLocalDate(Element element) {
-		Element dateTag = element.child(0);
-		if (dateTag != null) {
-			final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd. MMMM", Locale.GERMAN);
-			final int currentYear = LocalDate.now().getYear();
-			return MonthDay.parse(dateTag.ownText(), formatter).atYear(currentYear);
+	private LocalDate extractLocalDate(String text) {
+		int index = text.indexOf(' ');
+		index = text.indexOf(' ', index + 1);
+		index = text.indexOf(' ', index + 1);
+		text = text.substring(0, index);
+		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd. MMMM", Locale.GERMAN);
+		final int currentYear = LocalDate.now().getYear();
+		return MonthDay.parse(text, formatter).atYear(currentYear);
+	}
+
+	private String extractDj(String text) {
+		int index = text.toUpperCase().indexOf("MIT ");
+		int endIndex = text.toUpperCase().indexOf("EINTRITT");
+		if (index > 0 && endIndex > 0) {
+			return text.substring(index + 4, endIndex);
 		}
 		return null;
 	}
 
-	private String extractDanceEventTitle(Element paragraphElement) {
-		Elements elements = paragraphElement.nextElementSibling().getElementsByTag("h3");
-		if (elements != null && !elements.isEmpty()) {
-			return elements.get(0).childNode(0).attr("title");
+	private BigDecimal extractPrice(String text) {
+		int index = text.toUpperCase().indexOf("EINTRITT (CHF): ") + 16;
+		int endIndex = text.indexOf(' ', index);
+		if (index > 0 && endIndex > 0) {
+			return new BigDecimal(text.substring(index, endIndex));
 		}
 		return null;
 	}
