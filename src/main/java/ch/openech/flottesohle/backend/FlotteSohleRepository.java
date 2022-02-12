73e4d6765a683d52b9a4c0240e2afe9382d1e8dd
Bruno Eberhard <bruno.eberhard@pop.ch>
2022-02-12 10:15:27 +0100

    Ableiten statt delegieren, damit auch der TableCreator funktioniert

--- src/main/java/ch/openech/flottesohle/backend/FlotteSohleRepository.java
+++ src/main/java/ch/openech/flottesohle/backend/FlotteSohleRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.minimalj.model.Model;
-import org.minimalj.repository.Repository;
 import org.minimalj.repository.query.By;
 import org.minimalj.repository.query.Criteria;
 import org.minimalj.repository.query.FieldOperator;
@@ -21,34 +20,34 @@ import ch.openech.flottesohle.model.EventStatus;
  * Benutzer oder für sogar für jedes Filtern neu geladen werden.
  *
  */
-public class FlotteSohleRepository implements Repository {
-
-	private final Repository repository;
+public class FlotteSohleRepository extends SqlRepository {
 
 	private final Map<Object, DanceEvent> eventCache = new HashMap<>(1000);
 	private List<DanceEvent> events;
 	private long lastLoad = Long.MIN_VALUE;
 
 	public FlotteSohleRepository(Model model) {
-		this.repository = new SqlRepository(model);
+		super(model);
 	}
 
+	@SuppressWarnings("unchecked")
 	public <T> T read(Class<T> clazz, Object id) {
 		if (eventCache.containsKey(id)) {
 			return (T) eventCache.get(id);
 		}
-		T result = repository.read(clazz, id);
+		T result = super.read(clazz, id);
 		if (result instanceof DanceEvent) {
 			eventCache.put(id, (DanceEvent) result);
 		}
 		return result;
 	}
 
+	@SuppressWarnings("unchecked")
 	public <T> List<T> find(Class<T> clazz, Query query) {
 		if (query == EventsQuery.instance) {
 			LocalDate now = LocalDate.now();
 			if (events == null || lastLoad < System.currentTimeMillis() - 2 * 60 * 60 * 1000) {
-				events = repository.find(DanceEvent.class, By //
+				events = super.find(DanceEvent.class, By //
 						.field(DanceEvent.$.status, FieldOperator.notEqual, EventStatus.blocked) //
 						.and(By.field(DanceEvent.$.date, FieldOperator.greaterOrEqual, now)) //
 						.and(By.field(DanceEvent.$.date, FieldOperator.less, now.plusMonths(1)))
@@ -60,12 +59,12 @@ public class FlotteSohleRepository implements Repository {
 			events.removeIf(event -> event.location != null && event.location.isClosed(event.date));
 			return (List<T>) events;
 		} else {
-			return repository.find(clazz, query);
+			return super.find(clazz, query);
 		}
 	}
 
 	public <T> long count(Class<T> clazz, Criteria criteria) {
-		return repository.count(clazz, criteria);
+		return super.count(clazz, criteria);
 	}
 
 	private void clearCache() {
@@ -75,24 +74,24 @@ public class FlotteSohleRepository implements Repository {
 
 	public <T> Object insert(T object) {
 		events = null;
-		return repository.insert(object);
+		return super.insert(object);
 	}
 
 	public <T> void update(T object) {
 		clearCache();
-		repository.update(object);
+		super.update(object);
 	}
 
 	@Override
 	public <T> void delete(T object) {
 		clearCache();
-		repository.delete(object);
+		super.delete(object);
 	}
 	
 	@Override
 	public <T> int delete(Class<T> clazz, Criteria criteria) {
 		clearCache();
-		return repository.delete(clazz, criteria);
+		return super.delete(clazz, criteria);
 	}
 
 	public static class EventsQuery extends Query {
